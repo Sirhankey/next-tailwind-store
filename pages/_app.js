52a66f6d650b8be844005f246e72edec36ded18c
DESKTOP-1QSRUQV\Familia <dvmguimaraes@gmail.com>
2023-12-30 21:36:46 -0300

    Cria estrutura do CRUD

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import { PrimeReactProvider } from "primereact/api";
-
-import "../styles/globals.css";
+import '../styles/globals.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 
